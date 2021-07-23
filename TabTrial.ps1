7906bb90ff128dd540f4df4b5cb93e293684efaa
Markb5880 <scruffy_943@hotmail.com>
2021-07-23 20:07:32 +0100

    textbox6 changes

--- TabTrial.ps1
+++ TabTrial.ps1
@@ -1,5 +1,6 @@
 Add-Type -AssemblyName "System.Windows.Forms"
 Add-Type -AssemblyName "System.Drawing"
+[System.Reflection.Assembly]::LoadWithPartialName("System.Drawing.graphics")
 $Global:Form
 
 function TabFormTest {
@@ -18,7 +19,7 @@ cls
     $tabcontrol.height=512
     
     $FontFace = New-Object System.Drawing.Font(
-    "Comic Sans MS",24,[System.Drawing.FontStyle]::Regular)
+    "Comic Sans MS",10,[System.Drawing.FontStyle]::Regular)
     $tabcontrol.Font=$FontFace
 # Draw a Textbox
     $tabpage1=New-Object System.Windows.Forms.tabpage
@@ -28,17 +29,116 @@ cls
     $tabpage1.width=700
     $tabpage1.height=700
 
-    # Draw User To Delete Text Label
-    $textlbl1=New-Object System.Windows.Forms.label
-    $textlbl1.Location=New-Object System.Drawing.Size(220,150)
-    #$textlbl1.backcolor="Transparent"
-    $textlbl1.size=New-Object System.Drawing.Size(500,650)
-    $FontFace = New-Object System.Drawing.Font(
-    "Comic Sans MS",72,[System.Drawing.FontStyle]::Regular)
-    $textlbl1.Font=$FontFace
-    $textlbl1.text="HELLO"
+    #Get-ADPrincipalGroupMembership mbaker | select name
+ 
+    $dd=$null
+    
+    cls
+    
+    # Domain Textbox - top left
+        $TextBox6 = New-Object System.Windows.Forms.TextBox 
+        $TextBox6.Multiline = $false;
+        $TextBox6.Location = New-Object System.Drawing.Size(320,5) 
+        $TextBox6.Size = New-Object System.Drawing.Size(220,16)
+        $Font = New-Object System.Drawing.Font("Tahoma",10,[System.Drawing.FontStyle]::regular)
+        $TextBox6.Font=$Font
+        $gdm=get-addomain | select dnsroot
+        $gdm1=$gdm.dnsroot.tostring()
+            
+        $body1 = @"
+Current Domain: $gdm1
+"@
+        $Textbox6.text=$body1
 
+    # Scrollable User Selection Textbox - Mid Right
+        $TextBox2 = New-Object System.Windows.Forms.Listbox 
+        $TextBox2.Location = New-Object System.Drawing.Size(10,40) 
+        $TextBox2.Size = New-Object System.Drawing.Size(320,120)
+        $TextBox2.Height = 90
+    
+    # Scrollable Group Output Textbox - Bottom
+        $TextBox3 = New-Object System.Windows.Forms.textbox
+        $textbox3.multiline=$true
+        $textbox3.AutoSize=$false
+        $textbox3.scrollbars='vertical'
+        $TextBox3.Location = New-Object System.Drawing.Size(10,180) 
+        $TextBox3.Size = New-Object System.Drawing.Size(320,120)
+        $TextBox3.Height = 112
+    
+    # Draw Chosen Database Text Label
+        $textlbl1=New-Object System.Windows.Forms.label
+        $textlbl1.Location=New-Object System.Drawing.Size(5,139)
+        $textlbl1.backcolor="Transparent"
+        $textlbl1.size=New-Object System.Drawing.Size(72,18)
+        $FontFace = New-Object System.Drawing.Font(
+        "Comic Sans MS",12,[System.Drawing.FontStyle]::Regular)
+        $textlbl1.Font=$FontFace
+        $textlbl1.text="Username:"
+        
+    # Draw Chosen Database Textbox
+        $textbox1a=New-Object System.Windows.Forms.textbox
+        $textbox1a.Location=New-Object System.Drawing.Size(81,136)
+        $textbox1a.size=New-Object System.Drawing.Size(250,64)
+        $FontFace = New-Object System.Drawing.Font(
+        "Comic Sans MS",10,[System.Drawing.FontStyle]::Regular)
+        $textbox1a.Font=$FontFace
+     
+        $dname =@()
+    # Load the items and sort the list in Ascending order.
+        $usr=Get-ADUser -filter * -SearchBase "OU=F1,OU=TS Tech Users,DC=SMALLHOME,DC=LOCAL" | where {$_.enabled -eq $true } | select samaccountname,name |sort samaccountname
+        $usr.samaccountname | ForEach-Object {[void]$Textbox2.Items.Add($_);$dname=$usr.name}
+    # Current AD User Count
+        $TextBox5 = New-Object System.Windows.Forms.TextBox 
+        $TextBox5.Location = New-Object System.Drawing.Size(5,5) 
+        $TextBox5.Size = New-Object System.Drawing.Size(170,64)
+        $Font = New-Object System.Drawing.Font("Tahoma",10,[System.Drawing.FontStyle]::regular)
+        $TextBox5.Font=$Font
+        $usrcnt=$Textbox2.items.Count
+    
+        $body5 = @"
+        Current AD User Count:
+        $usrcnt
+"@
+        $Textbox5.text=$body5
+            
+       $Textbox2.add_click({$selection = $Textbox2.Selecteditem#;$Form.Controls.Clear($textbox1a)
+        $adp=Get-ADPrincipalGroupMembership -Identity $selection | sort name
+        $c=$adp.name # used for data line count
+        
+    # Display the 'users' groups, but we need to take care of the additional
+    # new line at the end of the list.
+        $lines=$c.count-1
+        $textbox3.Clear()
+    
+        foreach($gp in $adp){
+            $textbox3.appendtext($gp.samaccountname)
+                if ($lines -gt 0){
+                $textbox3.AppendText("`r`n")
+            $lines=$lines-1
+            }
+        }
+        $zz=$dname[$textbox2.SelectedIndex]
+        $textbox1a.Text="$zz"
+        
+        $textbox3.Refresh()
+        
+        
+        })
+    # Exit Button    
+        $Exitgbutton=New-Object System.Windows.Forms.Button
+        $Exitgbutton.Location = New-Object System.Drawing.Size(295,300)
+        $Exitgbutton.Size = New-Object System.Drawing.Size(100,32)
+        $Exitgbutton.Text = "Exit"
+        $Exitgbutton.add_click({$Form.Close()})
+    
+          
+    #}
+    $tabpage1.Controls.Add($textbox6)
+    $tabpage1.Controls.Add($textbox2)
+    $tabpage1.Controls.Add($textbox3)
     $tabpage1.Controls.Add($textlbl1)
+    $tabpage1.Controls.Add($textbox1a)
+    $tabpage1.Controls.Add($textbox5)
 
     $tabpage2=New-Object System.Windows.Forms.tabpage
     $tabpage2.name="Tab2"
@@ -53,7 +153,7 @@ cls
     #$textlbl1.backcolor="Transparent"
     $textlbl2.size=New-Object System.Drawing.Size(500,650)
     $FontFace = New-Object System.Drawing.Font(
-    "Comic Sans MS",72,[System.Drawing.FontStyle]::Regular)
+    "Comic Sans MS",12,[System.Drawing.FontStyle]::Regular)
     $textlbl2.Font=$FontFace
     $textlbl2.text="WORLD"
 
@@ -73,5 +173,5 @@ cls
     $form.Controls.Add($tabcontrol)
 
     $form.ShowDialog()
-    }
- TabFormTest
+}
+TabFormTest
