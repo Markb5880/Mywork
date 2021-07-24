e1886db2db5172c5134dc67abd0b40555b172df1
Markb5880 <scruffy_943@hotmail.com>
2021-07-24 23:15:14 +0100

    Added Presentation Framework for Windows msg boxes

--- TabTrial.ps1
+++ TabTrial.ps1
@@ -1,6 +1,7 @@
 Add-Type -AssemblyName "System.Windows.Forms"
 Add-Type -AssemblyName "System.Drawing"
 [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing.graphics")
+Add-Type -AssemblyName PresentationFramework
 $Global:Form
 
 function TabFormTest {
