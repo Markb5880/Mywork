45349852fdec6de7414fadcd2bd8bd204e35b4f6
Mark Baker <scruffy_943@hotmail.com>
2024-01-20 01:59:40 +0000

    dept issue

--- AD Powershell/Datagridview.ps1
+++ AD Powershell/Datagridview.ps1
@@ -19,15 +19,15 @@ $dataGridView.Columns[1].Name = "lastlogondate"
 $dataGridView.Columns[2].Name = "Date Acc Created"
 $dataGridView.Columns[3].Name = "PWD Last Set"
 $dataGridView.Columns[4].Name = "Enabled"
-
+$global:dept="accounts"
 $Button = New-Object System.Windows.Forms.Button
 $Button.Location = New-Object System.Drawing.Size(135,450)
 $Button.Size = New-Object System.Drawing.Size(120,25)
 $Button.Text = "Show Dialog Box"
-$Button.Add_Click({$dept="execs"})
+$Button.Add_Click({$global:dept="execs"})
 $Form.Controls.Add($Button)
 
-$row1 = Get-ADUser -Filter * -Properties * -SearchBase 'ou=$($dept),ou=administration,ou=f1,ou=ts tech users,dc=smallhome,dc=local' | select-object name,lastlogondate,whencreated,passwordlastset,enabled
+$row1 = Get-ADUser -Filter * -Properties * -SearchBase "ou=$($global:dept),ou=administration,ou=f1,ou=ts tech users,dc=smallhome,dc=local" | select-object name,lastlogondate,whencreated,passwordlastset,enabled
 foreach ($row in $row1)
 {    
 [void]$dataGridView.Rows.Add($row.name,$row.lastlogondate,$row.whencreated,$row.passwordlastset,$row.enabled)
