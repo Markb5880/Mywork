c2655ce140e9f8a6c77fed7c963302b32007fdc5
Mark Baker <scruffy_943@hotmail.com>
2024-01-20 17:35:27 +0000

    Lots of test changes

--- AD Powershell/Datagridview.ps1
+++ AD Powershell/Datagridview.ps1
@@ -1,37 +1,48 @@
-Function MakeNewForm {
-	MakeForm
-    $Form.Close()
-	$Form.Dispose()
-	
+function test1 {
+    test
 }
-function Makeform {
+function test {
+Add-Type -AssemblyName "System.Windows.Forms"
+Add-Type -AssemblyName "System.Drawing"
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName System.Collections
+
+$row1 = Get-ADUser -Filter * -Properties * -SearchBase "ou=accounts,ou=administration,ou=f1,ou=ts tech users,dc=smallhome,dc=local" | select-object name,lastlogondate,whencreated,passwordlastset,enabled
+
 $form = New-Object System.Windows.Forms.Form
-$form.Size = New-Object System.Drawing.Size(720,650)
-$dataGridView = New-Object System.Windows.Forms.DataGridView
-$dataGridView.Size=New-Object System.Drawing.Size(703,350)
+$form.Size = New-Object System.Drawing.Size(800,650)
 $form.Text='User Info'
-#$form.FormBorderStyle='Fixeddialog'
-$form.Controls.Add($dataGridView)
-$dataGridView.ColumnCount = 5
-$dataGridView.ColumnHeadersVisible = $true
-$dataGridView.Columns[0].Name = "UserName"
-$dataGridView.Columns[1].Name = "lastlogondate"
-$dataGridView.Columns[2].Name = "Date Acc Created"
-$dataGridView.Columns[3].Name = "PWD Last Set"
-$dataGridView.Columns[4].Name = "Enabled"
-$global:dept="accounts"
+
+#$tableData = New-Object System.Collections.ArrayList
+#$tableData.AddRange($row1)
+
+#$datagridview.DataBindings.DefaultDataSourceUpdateMode = 0 
+#$datagridview.DataSource= $TableData
+#$datagridview.Refresh()
+$dataGridView = New-Object System.Windows.Forms.DataGridView -Property @{
+    Size = New-Object System.Drawing.Size(650, 400)
+    Location = New-Object System.Drawing.Size(1, 10)
+    ColumnHeadersVisible = $True
+    DataSource = $row1
+    AutoSizeColumnsMode = 'AllCells'
+    ColumnHeadersHeightSizeMode = 'AutoSize'
+}  
+#$dataGridView.Columns[0].SortMode = DataGridViewColumnSortMode.automatic
+<#
 $Button = New-Object System.Windows.Forms.Button
 $Button.Location = New-Object System.Drawing.Size(135,450)
 $Button.Size = New-Object System.Drawing.Size(120,25)
 $Button.Text = "Show Dialog Box"
-$Button.Add_Click({$global:dept="execs"})
+$Button.Add_Click({
+    $dataGridView.DataSource = $null
+    #$dataGridView.Rows.Clear()
+    $row1 = Get-ADUser -Filter * -Properties * -SearchBase "ou=execs,ou=administration,ou=f1,ou=ts tech users,dc=smallhome,dc=local" | select-object name,lastlogondate,whencreated,passwordlastset,enabled
+    $dataGridView.DataSource=$row1})
+#>
+$Form.Controls.Add($dataGridView)
 $Form.Controls.Add($Button)
+  
+$Form.ShowDialog()
 
-$row1 = Get-ADUser -Filter * -Properties * -SearchBase "ou=$($global:dept),ou=administration,ou=f1,ou=ts tech users,dc=smallhome,dc=local" | select-object name,lastlogondate,whencreated,passwordlastset,enabled
-foreach ($row in $row1)
-{    
-[void]$dataGridView.Rows.Add($row.name,$row.lastlogondate,$row.whencreated,$row.passwordlastset,$row.enabled)
 }
-$form.ShowDialog()
-}
-MakeNewform
+test1
