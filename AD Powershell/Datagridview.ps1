c6312f5a8337dd608894719b8d9be658a1221b89
Mark Baker <scruffy_943@hotmail.com>
2024-01-20 01:21:07 +0000

    New file

---
+++ AD Powershell/Datagridview.ps1
@@ -0,0 +1,38 @@
+Function MakeNewForm {
+	MakeForm
+    $Form.Close()
+	$Form.Dispose()
+	
+}
+function Makeform {
+$form = New-Object System.Windows.Forms.Form
+$form.Size = New-Object System.Drawing.Size(720,650)
+$dataGridView = New-Object System.Windows.Forms.DataGridView
+$dataGridView.Size=New-Object System.Drawing.Size(703,350)
+$form.Text='User Info'
+#$form.FormBorderStyle='Fixeddialog'
+$form.Controls.Add($dataGridView)
+$dataGridView.ColumnCount = 5
+$dataGridView.ColumnHeadersVisible = $true
+$dataGridView.Columns[0].Name = "UserName"
+$dataGridView.Columns[1].Name = "lastlogondate"
+$dataGridView.Columns[2].Name = "Date Acc Created"
+$dataGridView.Columns[3].Name = "PWD Last Set"
+$dataGridView.Columns[4].Name = "Enabled"
+
+$Button = New-Object System.Windows.Forms.Button
+$Button.Location = New-Object System.Drawing.Size(135,450)
+$Button.Size = New-Object System.Drawing.Size(120,25)
+$Button.Text = "Show Dialog Box"
+$Button.Add_Click({$dept="execs"})
+$Form.Controls.Add($Button)
+
+
+$row1 = Get-ADUser -Filter * -Properties * -SearchBase 'ou=$($dept),ou=administration,ou=f1,ou=ts tech users,dc=smallhome,dc=local' | select name,lastlogondate,whencreated,passwordlastset,enabled
+foreach ($row in $row1)
+{    
+[void]$dataGridView.Rows.Add($row.name,$row.lastlogondate,$row.whencreated,$row.passwordlastset,$row.enabled)
+}
+$form.ShowDialog()
+}
+MakeNewform
